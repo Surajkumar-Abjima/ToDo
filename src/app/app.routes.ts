beee41e02d6eaa29231d70f7ff1fb06619753446
Suraj Kumar <suraj.kumar@abjima.com>
2025-03-29 14:51:52 +0530

    Forth commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { KanbanBoardComponent } from './kanban/kanban-board/kanban-board.component';
 
 export const routes: Routes = [
     {path:'', component:TodoListComponent},
-    {path:'addTask', component:FormComponent}
+    {path:'addTask', component:FormComponent},
+    {path:'kanban',component:KanbanBoardComponent}
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TodoListComponent } from '../todo-list/todo-list.component';
 
 @Component({
   selector: 'app-navbar',
